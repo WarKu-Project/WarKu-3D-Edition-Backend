ea97681d91062d89ddb2713031c44b62a03fb87e
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-05-15 20:12:57 +0700

    Add Terminate Condition

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,31 @@
 /**
+* Import MongoDB
+**/
+let mongodb = require('../mongodb')
+/**
 * Import Server Components
 **/
 let packet = require('./packet')
 let Remote = require('./remote')
+let server = require('dgt-net').server
 /**
 * Init Server
 **/
-let server = require('dgt-net').server
-server.setRemoteProxyClass(Remote)
-server.setPacketObject(packet)
-server.listen(9000)
+server.init = ()=>{
+  server.setRemoteProxyClass(Remote)
+  server.setPacketObject(packet)
+  server.listen(process.PORT)
+  mongodb.update('server',{ type:'world'},{port:process.PORT,status:'Running'},()=>{
+    server.startProcessCounting()
+  })
+}
+/**
+* Start Update time to server
+**/
+server.startProcessCounting = ()=>{
+  let time = 0
+  setInterval(()=>{
+    mongodb.update('server',{type:'world',port:process.PORT},{time:++time,response:0})
+  },1000)
+}
+module.exports = server
